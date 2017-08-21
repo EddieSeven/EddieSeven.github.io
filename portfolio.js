b40a8d5a2e74768a01066bdb6d4748435ef6d80a
Charles Culpepper <charlescd7@gmail.com>
2017-08-21 11:52:10 -0600

    Site wide change to 2018 Rdesign
    New site done in Bootstrap 3. Old site, renamed so it can be accessed
    and linked to if needed. Relevant JS and CSS files were aldo
    added/updated.

---
+++ portfolio.js
@@ -0,0 +1,5 @@
+var buckets = document.getElementsByClassName("buckets");
+    blurbs = document.getElementsByClassName("blurbs");
+
+TweenLite.from(buckets, 0.7, {opacity:0, y:15});
+TweenLite.from(blurbs, 1.8, {opacity:0, y:7});
