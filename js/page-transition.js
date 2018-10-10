948209d7b3950e37b2ad40c93cc628eb35227164
Eddie Seven <charlescd7@gmail.com>
2018-10-09 18:17:48 -0600

    Delete process to prep for redesign upload.

--- js/page-transition.js
+++
@@ -1,5 +0,0 @@
-var PageTransitions = (function() {
-
-	var $main = $( '#main' )
-    
-    })();
--- portfolio.js
+++
@@ -1,5 +0,0 @@
-var buckets = document.getElementsByClassName("buckets");
-    blurbs = document.getElementsByClassName("blurbs");
-
-TweenLite.from(buckets, 0.7, {opacity:0, y:15});
-TweenLite.from(blurbs, 1.8, {opacity:0, y:7});
